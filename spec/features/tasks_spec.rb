3d7619e38e7b6e37ccb1c7635d85dbfc6776e96a
Joanna Chien <joannachou713@gmail.com>
2021-06-11 14:33:24 +0800

    #11 add feature spec

--- spec/features/tasks_spec.rb
+++ spec/features/tasks_spec.rb
@@ -1,53 +1,67 @@
 require 'rails_helper'
 
 RSpec.feature "Tasks", type: :feature do
-  context "create new user" do
+  context "create new tasks" do
     before(:each) do
       visit new_task_path
       within('form') do
-        choose I18n.t('priority')[0]
-        choose I18n.t('status')[0]
+        choose 'task_priority_2'
+        choose 'task_status_0'
       end
     end
 
 
     scenario "should be successful" do
       within('form') do
-        fill_in I18n.t('shoe.title'), with: 'new task test'
+        fill_in 'task_title', with: 'new task test'
       end
-      click_button 'Create Task'
-      expect(page).to have_content('Task was successfully created')
+      click_button 'commit'
+      expect(page).to have_content(I18n.t('successful-create'))
     end
     
     
     scenario "should fail" do
-      click_button 'Create Task'
-      expect(page).to have_content('Title can\'t be blank')
+      click_button 'commit'
+      expect(page).to have_content(I18n.t('errors.messages.blank'))
     end
   end
 
 
-  context "update user" do
+  context "update tasks" do
     scenario "should be successful" do
       task = Task.create(id: 1, title: 'title', content: 'string', start: DateTime.now, end: (DateTime.now + 1.week), priority: 0, status: 0)
       visit edit_task_path(task)
       within('form') do
-        fill_in I18n.t('shoe.title'), with: '測試修改'
+        fill_in 'task_title', with: '測試修改'
       end
-      click_button 'Update Task'
-      expect(page).to have_content('Task was successfully updated')
+      click_button 'commit'
+      expect(page).to have_content(I18n.t('successful-update'))
       expect(page).to have_content('測試修改')
     end
   end
   
   
-  context "destory user" do
+  context "destory tasks" do
     scenario "should be successful" do
       task = Task.create(id: 1, title: 'title', content: 'string', start: DateTime.now, end: (DateTime.now + 1.week), priority: 0, status: 0)
       visit tasks_path
-      # click_link '刪除'
-      expect{ click_link 'Delete' }.to change(Task, :count).by(-1)
-      expect(page).to have_content('Task was successfully deleted')
+      expect{ click_link I18n.t('delete') }.to change(Task, :count).by(-1)
+      expect(page).to have_content(I18n.t('successful-delete'))
     end
   end
+
+
+  context "sort tasks" do
+    scenario "sort by creation time" do
+      task = Task.create(id: 1, title: 'title1', content: 'string', created_at: DateTime.now, start: DateTime.now, end: (DateTime.now + 1.week), priority: 0, status: 0)
+      task1 = Task.create(id: 0, title: 'title0', content: 'string', created_at: DateTime.now+1.hour, start: DateTime.now, end: (DateTime.now + 1.week), priority: 0, status: 0)
+      visit tasks_path
+      expect(page).to have_content(/title0.+title1/)
+      
+      visit sort_create_url
+      expect(page).to have_content(/title1.+title0/)
+    end
+  end
+  
+
 end
