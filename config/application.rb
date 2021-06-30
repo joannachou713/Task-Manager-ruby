f83068faf588c5bf22ca3f7a09bd597bf16d22e0
Joanna Chien <joannachou713@gmail.com>
2021-06-30 17:06:17 +0800

    #24 add 404 & 500 page

--- config/application.rb
+++ config/application.rb
@@ -12,6 +12,7 @@ module TaskManagerRuby
     config.load_defaults 6.1
     config.i18n.default_locale = "zh-TW"
     config.time_zone = "Taipei"
+    config.exceptions_app = self.routes
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
